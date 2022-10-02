6e21b20a532b94159c66742ec24d755203b3d83b
ola-torinmo <adebamzy@gmail.com>
2022-10-02 08:14:04 +0100

    more config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    './components/**/*.{html,js}',
+    './pages/**/*.{html,js}',
+    './dist/index.html',
+  ],
   theme: {
     extend: {},
   },
